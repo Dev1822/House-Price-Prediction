55b6669cf0261679858940f6e1d9b995d609ce79
Dev1822 <dev.d.patel.cg@gmail.com>
2026-03-19 23:11:45 +0530

    final fix: gdown model loading

--- backend/app.py
+++ backend/app.py
@@ -3,18 +3,28 @@ import joblib
 import numpy as np
 from flask_cors import CORS
 import os
+import gdown
 
 app = Flask(__name__)
 CORS(app)
 
-model_path = os.path.join(os.path.dirname(__file__), "model", "model.joblib")
+model_path = os.path.join(os.path.dirname(__file__), "model.joblib")
+
+file_id = "1qToEJhC7-PSmEK52sft34K7rl0EGRW5J"
+
+if not os.path.exists(model_path):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, model_path, quiet=False)
+
 model = joblib.load(model_path)
+
 
 @app.route("/")
 def home():
     return "Flask is running"
 
-@app.route("/predict",methods=["POST"])
+
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.json["features"]
 
@@ -37,10 +47,11 @@ def predict():
     ]
 
     prediction = model.predict([features])
-    
+
     return jsonify({
         "prediction": int(prediction[0])
     })
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
